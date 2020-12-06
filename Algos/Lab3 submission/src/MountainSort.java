205796819164266dbc9cdab81c4854043e864d70
Harrison Barker <harrison@thebarkers.me.uk>
2020-12-06 23:46:47 +0000

    algos lab 3, software project: uml diagrams, Functional spec, tech arch, sys req, scope, ethical, eval

--- Algos/Lab3 submission/src/MountainSort.java
+++ Algos/Lab3 submission/src/MountainSort.java
@@ -20,12 +20,14 @@ public class MountainSort
         int[] bottom = new int[A.length / 2];
         int[] top = new int[A.length / 2];
 
+        // copy the two halves into seperate arrays
         System.arraycopy(A, 0, bottom, 0, bottom.length);
         System.arraycopy(A, bottom.length, top, 0, top.length);
 
         bottom = CountingSort(bottom, true);
         top = CountingSort(top, false);
 
+        // copy the two sorted halves back into the original array
         System.arraycopy(bottom, 0, A, 0, bottom.length);
         System.arraycopy(top, 0, A, top.length, top.length);
 
@@ -37,7 +39,6 @@ public class MountainSort
         int size = A.length;
         int[] output = new int[size + 1];
 
-        // Find the largest element of the array
         int max = A[0];
         for (int i = 1; i < size; i++)
         {
@@ -46,26 +47,21 @@ public class MountainSort
         }
         int[] count = new int[max + 1];
 
-        // Initialize count array with all zeros.
         for (int i = 0; i < max; ++i)
         {
             count[i] = 0;
         }
 
-        // Store the count of each element
         for (int i = 0; i < size; i++)
         {
             count[A[i]]++;
         }
 
-        // Store the cummulative count of each array
         for (int i = 1; i <= max; i++)
         {
             count[i] += count[i - 1];
         }
 
-        // Find the index of each element of the original array in count array, and
-        // place the elements in output array
         for (int i = size - 1; i >= 0; i--)
         {
             output[count[A[i]] - 1] = A[i];
